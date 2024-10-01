68ecbe03be4b1790c31b255bdbf94314ac1a46c2
Mehmet Fidan <mehmetfidan2090@gmail.com>
2024-10-01 15:21:16 +0300

    errorHandling added & HTTP Post added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Cart from "./components/Cart/Cart";
+import FormInputs from "./components/Form/FormInputs";
 import Hero from "./components/Hero/Hero";
 import Header from "./components/Layout/Header";
 import Products from "./components/Products/Products";
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,40 +1,66 @@
-// import products from "../../productData.js";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import FormInputs from "../Form/FormInputs.jsx";
 import ProductItem from "./ProductItem.jsx";
 import "./Products.css";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const productList = products.map((product) => (
-    <ProductItem key={product.id} product={product}>
-      {product.name}
-    </ProductItem>
-  ));
+  const productList = products
+    .map((product) => (
+      <ProductItem key={product.id} product={product}>
+        {product.name}
+      </ProductItem>
+    ))
+    .reverse();
 
-  const fetchProductsHandler = async () => {
+  const fetchProductsHandler = useCallback(async function () {
     setIsLoading(true);
-    const response = await fetch(
-      "https://my-pos-application-api.onrender.com/api/products/get-all"
-    );
-    const data = await response.json();
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://my-pos-application-api.onrender.com/api/products/get-all"
+      );
 
-    const newData = data.map((item) => {
-      return {
-        id: item._id,
-        name: item.title,
-        ...item,
-      };
-    });
-    setProducts(newData);
+      if (response.status !== 200) {
+        throw new Error("Something went wrong!!");
+      }
+
+      const data = await response.json();
+      const newData = data.map((item) => {
+        return {
+          id: item._id,
+          name: item.title,
+          ...item,
+        };
+      });
+      setProducts(newData);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
-  };
+  }, []);
+  useEffect(() => {
+    fetchProductsHandler();
+  }, [fetchProductsHandler]);
+
+  let content = <p>Found No Products!!</p>;
+  if (products.length > 0) {
+    content = productList;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <main className="products-wrapper">
-      <ul className="products">{!isLoading && productList}</ul>
-      {isLoading && <p>Loading...</p>}
-      {!isLoading && products.length === 0 && <p>Found No Products!!</p>}
+      <FormInputs fetchProductsHandler={fetchProductsHandler} />
+      <ul className="products">{content}</ul>
       <button className="button" onClick={fetchProductsHandler}>
         Fetch Products
       </button>
